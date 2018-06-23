03056950cd9ef6e93c9456d381d2e2dfc11f4252
Zviad Sichinava <zviad.sichinava@devfactory.com>
2018-06-23 17:50:22 +0200

    chore: created file upload component

--- src/app/modules/customer-statement/customer-statement.module.ts
+++ src/app/modules/customer-statement/customer-statement.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { CustomerStatementRoutingModule } from './customer-statement-routing.module';
 import { UploadStatementComponent } from './pages/upload-statement/upload-statement.component';
+import { FileUploadComponent } from './components/file-upload/file-upload.component';
 
 @NgModule({
   imports: [
@@ -10,7 +11,8 @@ import { UploadStatementComponent } from './pages/upload-statement/upload-statem
     CustomerStatementRoutingModule
   ],
   declarations: [
-    UploadStatementComponent
+    UploadStatementComponent,
+    FileUploadComponent
   ]
 })
 export class CustomerStatementModule { }
