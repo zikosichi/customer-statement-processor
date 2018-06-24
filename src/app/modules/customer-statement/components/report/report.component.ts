3a717fcd4c367da6396a75ffb3e54b8ac5a4b4f1
Zviad Sichinava <zviad.sichinava@devfactory.com>
2018-06-24 19:19:11 +0200

    chore: validate selected statement

--- src/app/modules/customer-statement/components/report/report.component.ts
+++ src/app/modules/customer-statement/components/report/report.component.ts
@@ -7,17 +7,38 @@ import { Statement } from '../../models/statement';
 @Component({
   selector: 'app-report',
   templateUrl: './report.component.html',
-  styleUrls: ['./report.component.css']
+  styleUrls: ['./report.component.scss']
 })
 export class ReportComponent implements OnInit {
 
+  _statements: Statement[] = [];
   @Input()
-  statements: Statement[] = [];
+  set statements(value) {
+    this._statements = value;
+    this.validateStatements();
+  }
+  get statements(): Statement[] {
+    return this._statements;
+  }
 
   constructor() { }
 
   ngOnInit() {
-    console.log(this.statements);
   }
 
+  /**
+   * Validate statements
+   */
+  validateStatements() {
+    // sort by reference to find duplicates
+    this.statements.sort((a, b) => a.reference > b.reference ? 1 : -1);
+
+    this.statements.forEach((s, index) => {
+      // validate balance
+      s.isInvalidBalance = s.startBalance + s.mutation !== s.endBalance;
+
+      // validate duplicates
+      s.isDuplicate = this.statements.filter(statement => statement.reference === s.reference).length > 1;
+    });
+  }
 }
