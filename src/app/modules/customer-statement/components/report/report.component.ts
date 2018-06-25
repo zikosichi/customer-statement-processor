1c4fbabd9b85672ab356a5638604d6067a039e46
Zviad Sichinava <zviad.sichinava@devfactory.com>
2018-06-25 10:37:31 +0200

    chore: add validation messages

--- src/app/modules/customer-statement/components/report/report.component.ts
+++ src/app/modules/customer-statement/components/report/report.component.ts
@@ -11,6 +11,9 @@ import { Statement } from '../../models/statement';
 })
 export class ReportComponent {
 
+  invalidBalanceCount: number;
+  duplicatesCount: number;
+
   _statements: Statement[] = [];
   @Input()
   set statements(value) {
@@ -25,15 +28,21 @@ export class ReportComponent {
    * Validate statements
    */
   validateStatements() {
+    // Reset errors
+    this.invalidBalanceCount = 0;
+    this.duplicatesCount = 0;
+
     // sort by reference so it's easy for the user to capture duplicates
     this.statements.sort((a, b) => a.reference > b.reference ? 1 : -1);
 
     this.statements.forEach((s, index) => {
       // validate balance
       s.isInvalidBalance = (s.startBalance + s.mutation).toFixed(12) !== s.endBalance.toFixed(12);
+      this.invalidBalanceCount = s.isInvalidBalance ? this.invalidBalanceCount + 1 : this.invalidBalanceCount;
 
       // validate duplicates
       s.isDuplicate = this.statements.filter(statement => statement.reference === s.reference).length > 1;
+      this.duplicatesCount = s.isDuplicate ? this.duplicatesCount + 1 : this.duplicatesCount;
     });
   }
 }
