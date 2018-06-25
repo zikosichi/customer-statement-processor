d72e3b482e0eef791781e9102974b4901f544ab8
Zviad Sichinava <zviad.sichinava@devfactory.com>
2018-06-25 12:23:30 +0200

    refactor: file parser service spec

--- src/app/modules/customer-statement/services/file-parser.service.spec.ts
+++ src/app/modules/customer-statement/services/file-parser.service.spec.ts
@@ -17,61 +17,27 @@ describe('UtilsService', () => {
   }));
 });
 
-describe('ParseXML', () => {
-  const testXml: string = `<records>
-  <record reference="168036">
-    <accountNumber>NL91RABO0315273637</accountNumber>
-    <description>Tickets for Rik Bakker</description>
-    <startBalance>85.69</startBalance>
-    <mutation>-19.48</mutation>
-    <endBalance>66.21</endBalance>
-  </record>
-  <record reference="168512">
-    <accountNumber>NL90ABNA0585647886</accountNumber>
-    <description>Flowers from Peter King</description>
-    <startBalance>5429</startBalance>
-    <mutation>-939</mutation>
-    <endBalance>6368</endBalance>
-  </record>
-</records>`;
-
-  const correspondingJson: Statement[] = [
-    {
-      reference: '168036',
-      accountNumber: 'NL91RABO0315273637',
-      description: 'Tickets for Rik Bakker',
-      startBalance: 85.69,
-      mutation: -19.48,
-      endBalance: 66.21,
-    },
-    {
-      reference: '168512',
-      accountNumber: 'NL90ABNA0585647886',
-      description: 'Flowers from Peter King',
-      startBalance: 5429,
-      mutation: -939,
-      endBalance: 6368,
-    },
-  ];
-
-  let service: FileParserService;
-
-  beforeEach(() => {
-    service = new FileParserService();
-  });
-
-  it('Should parse the XML file and return corresponding JSON', () => {
-    service.parseXml(testXml).subscribe(res => {
-      expect(res).toEqual(correspondingJson);
-    });
-
-  });
-});
-
 describe('ParseCSV', () => {
-  const testXml: string = `Reference,Account Number,Description,Start Balance,Mutation,End Balance
+  const testCsv: string = `Reference,Account Number,Description,Start Balance,Mutation,End Balance
 106627,NL46ABNA0625805417,Subscription for Erik de Vries,80.76,-2.23,78.5
 112806,NL74ABNA0248990274,Candy from Jan de Vries,109.75,-23.94,85.81`;
+
+  const testXml: string = `<records>
+    <record reference="106627">
+      <accountNumber>NL46ABNA0625805417</accountNumber>
+      <description>Subscription for Erik de Vries</description>
+      <startBalance>80.76</startBalance>
+      <mutation>-2.23</mutation>
+      <endBalance>78.5</endBalance>
+    </record>
+    <record reference="112806">
+      <accountNumber>NL74ABNA0248990274</accountNumber>
+      <description>Candy from Jan de Vries</description>
+      <startBalance>109.75</startBalance>
+      <mutation>-23.94</mutation>
+      <endBalance>85.81</endBalance>
+    </record>
+  </records>`;
 
   const correspondingJson: Statement[] = [
     {
@@ -99,7 +65,13 @@ describe('ParseCSV', () => {
   });
 
   it('Should parse the CSV file and return corresponding JSON', () => {
-    service.parseCsv(testXml).subscribe(res => {
+    service.parseCsv(testCsv).subscribe(res => {
+      expect(res).toEqual(correspondingJson);
+    });
+  });
+
+  it('Should parse the XML file and return corresponding JSON', () => {
+    service.parseXml(testXml).subscribe(res => {
       expect(res).toEqual(correspondingJson);
     });
   });
